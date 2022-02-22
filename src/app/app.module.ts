5891a19331d362b010531cc83e54877be797b70f
Karim <mail@ackarim.com>
2022-02-22 12:40:48 +0100

    Starting beeta component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BetaComponent } from './beta/beta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BetaComponent
   ],
   imports: [
     BrowserModule
