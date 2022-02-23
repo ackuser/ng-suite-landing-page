58cd94d9e7695acd9a78727ab32018125d5cdc79
Karim <mail@ackarim.com>
2022-02-23 16:18:07 +0100

    Componentizing app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BetaComponent } from './beta/beta.component';
+import { FigureComponent } from './figure/figure.component';
+import { FiguresComponent } from './figures/figures.component';
+import { FooterComponent } from './footer/footer.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BetaComponent
+    BetaComponent,
+    FigureComponent,
+    FiguresComponent,
+    FooterComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/figure/figure.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-figure',
+  templateUrl: './figure.component.html',
+  styleUrls: ['./figure.component.scss'],
+  // encapsulation: ViewEncapsulation.None,
+})
+export class FigureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
